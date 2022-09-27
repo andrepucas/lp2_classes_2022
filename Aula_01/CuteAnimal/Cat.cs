e67e1288b03dab6c413d4953c308a6b7ea5978b6
andrepucas <andre.pucas.santos@gmail.com>
2022-09-27 11:38:41 +0100

    Exercise 3

---
+++ Aula_01/CuteAnimal/Cat.cs
@@ -0,0 +1,10 @@
+namespace CuteAnimal
+{
+    public class Cat
+    {
+        private string  _name;
+        private uint    _age;
+        private Feed    _feedStatus;
+        private Mood    _moodStatus;
+    }
+}
