8cfafaa3c62b9d7c6fd51d5f360f68c150b675ce
andrepucas <andre.pucas.santos@gmail.com>
2022-09-27 12:31:06 +0100

    Exercise 4

--- Aula_01/CuteAnimal/Cat.cs
+++ Aula_01/CuteAnimal/Cat.cs
@@ -1,10 +1,39 @@
+using System;
+
 namespace CuteAnimal
 {
     public class Cat
     {
         private string  _name;
-        private uint    _age;
+        private int     _age;
         private Feed    _feedStatus;
         private Mood    _moodStatus;
+        private Random  _random;
+
+        private Cat()
+        {
+            _random = new Random();
+        }
+
+        public Cat(string name, int age, Feed feedStatus, Mood moodStatus) : this()
+        {
+            _name = name;
+            _age = age;
+            _feedStatus = feedStatus;
+            _moodStatus = moodStatus;
+        }
+
+        public Cat(string name) : this()
+        {
+            _name = name;
+            _age = _random.Next(25);
+            _feedStatus = (Feed)_random.Next(Enum.GetNames(typeof(Feed)).Length);
+            _moodStatus = (Mood)_random.Next(Enum.GetNames(typeof(Mood)).Length);
+        }
+
+        public string GetName() => _name;
+
+        public (string, int, Feed, Mood) GetAll() 
+            => (_name, _age, _feedStatus, _moodStatus);
     }
 }
--- Aula_01/CuteAnimal/Program.cs
+++ Aula_01/CuteAnimal/Program.cs
@@ -1,10 +1,20 @@
+using System;
+
 namespace CuteAnimal
 {
     public class Program
     {
         static void Main()
         {
-            // BEEP BOOP.
+            Cat c1 = new Cat("Beep", 3, Feed.Satisfied, Mood.IgnoringYou);
+            Cat c2 = new Cat("Clap", 2, Feed.Hungry, Mood.Grumpy);
+            Cat c3 = new Cat ("R1");
+            Cat c4 = new Cat ("R2");
+
+            Console.WriteLine("Cat 1: " + c1.GetAll());
+            Console.WriteLine("Cat 2: " + c2.GetAll());
+            Console.WriteLine("Cat 3 (Random): " + c3.GetAll());
+            Console.WriteLine("Cat 4 (Random): " + c4.GetAll());
         }
     }
 }
