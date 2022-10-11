aadc97fbb4151d50ea1ff1a654dda3031e6be719
Pucas <andre.pucas.santos@gmail.com>
2022-10-11 19:41:40 +0100

    Exercise 3

---
+++ Aula_03/CityManager1/Building.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityManager1
+{
+    public class Building : IHasValue
+    {
+        public string Type {get;}
+        public float Area {get;}
+        public float Value {get;}
+
+        public Building(string type, float area, float value)
+        {
+            Type = type;
+            Area = area;
+            Value = value;
+        }
+
+        public bool Equals (IHasValue other)
+        {
+            if (other == null)
+                return false;
+
+            else return other.Value == Value;
+        }
+
+        public override string ToString() => 
+            $"{Type, -20} | {Value, 8:f2} | {Area, 8:f2}";
+        
+    }
+}
