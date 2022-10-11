111f74883bbdc913cdf4d61512c4e001bd0e5a0b
Pucas <andre.pucas.santos@gmail.com>
2022-10-11 22:04:44 +0100

    Exercicio 10

---
+++ Aula_03/MyGenericMethod/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyGenericMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IHasValue[] valuables = new IHasValue[]
+            {
+                new Building("T3", 120, 3000),
+                new Building("T0", 5, 1000),
+                new MilitaryUnit(3, 50, 10),
+                new SettlerUnit(5, 200),
+                new SettlerUnit(4, 200)
+            };
+
+            Console.WriteLine("No. buildings       : {0}",
+                HowManyOfType<Building>(valuables));
+
+            Console.WriteLine("No. units           : {0}",
+                HowManyOfType<Unit>(valuables));
+
+            Console.WriteLine("No. settler units   : {0}",
+                HowManyOfType<SettlerUnit>(valuables));
+
+            Console.WriteLine("No. military units  : {0}",
+                HowManyOfType<MilitaryUnit>(valuables));
+        }
+
+        private static int HowManyOfType<T>(IEnumerable<IHasValue> p_collection)
+            where T : IHasValue
+        {
+            int __count = 0;
+            foreach (IHasValue item in p_collection) if (item is T) __count++;
+            return __count;
+        }
+    }
+}
---
+++ Aula_03/MyGenericMethod/Unit.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace MyGenericMethod
+{
+    public abstract class Unit : IHasValue
+    {
+        private int movement;
+        public virtual int Health { get; set; }
+        public abstract float Value { get; }
+
+        protected Unit(int movement, int health)
+        {
+            this.movement = movement;
+            Health = health;
+        }
+
+        public void Move(Vector2 v)
+        {
+            // Assumindo vizinhança Von Neumann
+            int d = Math.Abs(v.X) + Math.Abs(v.Y);
+
+            // int d = (v.X >= 0 ? v.X : -v.X)
+            //     + (v.Y >= 0 ? v.Y : -v.Y);
+
+            Console.WriteLine(
+                $"{this.GetType().Name} has moved {d} positions");
+        }
+
+        public bool Equals (IHasValue other)
+        {
+            if (other == null)
+                return false;
+
+            else return other.Value == Value;
+        }
+
+        public override string ToString() =>
+            $"HP={Health}, Val={Value}";
+    }
+}
