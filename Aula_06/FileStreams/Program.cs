311d6b368b41171ae20cbb45ca6c8ea9db0362ef
Pucas <andre.pucas.santos@gmail.com>
2022-11-08 12:39:22 +0000

    Add exercicio 4

--- Aula_06/FileStreams/Program.cs
+++ Aula_06/FileStreams/Program.cs
@@ -9,6 +9,15 @@ namespace FileStreams
         // Nomes dos ficheiros
         private const string filenameText = "dados.txt";
         private const string filenameBinary = "dados.bin";
+        
+        // Paths dos ficheiros
+        private static readonly string textPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                filenameText);
+
+        private static readonly string binPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                filenameBinary);
 
         // Dados a escrever e ler nos ficheiros
         private const string dataString = "Hello world!";
@@ -64,59 +73,43 @@ namespace FileStreams
         // 1. Escreve ficheiro em modo de texto
         private static void EscreverTexto()
         {
-            StreamWriter sw = File.CreateText(Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                filenameText));
+            using StreamWriter sw = File.CreateText(textPath);
 
             sw.WriteLine(dataString);
             sw.WriteLine(dataInt);
             sw.WriteLine(dataFloat);
-
-            sw.Close();
         }
 
         // 2. Lê ficheiro em modo de texto
         private static void LerTexto()
         {
-            StreamReader sr = File.OpenText(Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                filenameText));
+            using StreamReader sr = File.OpenText(textPath);
 
             string impStringData = sr.ReadLine();
             int impIntData = Convert.ToInt32(sr.ReadLine());
             float impFloatData = Convert.ToSingle(sr.ReadLine());
 
-            sr.Close();
-
             Console.WriteLine("Imported string: " + impStringData);
             Console.WriteLine("Imported int: " + impIntData);
             Console.WriteLine("Imported float: " + impFloatData);
+
         }
 
         // 3. Escreve ficheiro em modo binário
         private static void EscreverBin()
         {
-            string path = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                filenameBinary);
-
-            BinaryWriter bw = new BinaryWriter(File.Create(path));
+            using BinaryWriter bw = new BinaryWriter(File.Create(binPath));
 
             bw.Write(dataString);
             bw.Write(dataInt);
             bw.Write(dataFloat);
-
-            bw.Close();
         }
 
         // 4. Lê ficheiro em modo binário
         private static void LerBin()
         {
-            string path = Path.Combine(
-                Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
-                filenameBinary);
-
-            BinaryReader br = new BinaryReader(File.Open(path, FileMode.Open));
+            using BinaryReader br = new BinaryReader(
+                File.Open(binPath, FileMode.Open));
 
             string impStringData = br.ReadString();
             int impIntData = br.ReadInt32();
