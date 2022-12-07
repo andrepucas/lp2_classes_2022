6e12543cb399504e14b38ccc6573fd102804606f
Pucas <andre.pucas.santos@gmail.com>
2022-12-07 13:34:45 +0000

    Renames

--- Aula_04/AulasUnity/Assets/Scripts/Aula09/IAnimateBehaviour.cs
+++
@@ -1,4 +0,0 @@
-﻿public interface IAnimateBehaviour
-{
-    void Animate();
-}
--- Aula_04/AulasUnity/Assets/Scripts/Aula09/RotateBehaviour.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class RotateBehaviour : MonoBehaviour, IAnimateBehaviour
-{
-    public void Animate() => transform.Rotate(Vector3.one);
-}
